1f506c91d9901220d4781264d0288c15f7708546
JAI <jai19kharb@gmail.com>
2025-10-04 23:29:36 +0530

    module 4

--- dataset_dataloader.py
+++ dataset_dataloader.py
@@ -18,9 +18,27 @@ dataset = SimpleDataset() #instance of custom dataset
 #gives 4 sample per batch, randomizes order each epoch(shuffle)
 #dataloader for dataset
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+# this line handles batching, shuffling and parallel loading
+#bridge between raw data and model
 
 # this loops over the dataloader, each iteration gives a batch of data and labels
 for batch_idx, (data,labels) in enumerate(dataloader):
     print(f"batch {batch_idx}:")
     print("data: ", data)
     print("labels: ", labels)
+
+
+'''for epoch in range(10):
+    for data, labels in dataloader:
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        this is used in actual training loops. 
+
+        The Dataset defines what the data is,
+    and the DataLoader defines how it’s fed to the model — 
+    in batches, shuffled, efficiently.
+'''
