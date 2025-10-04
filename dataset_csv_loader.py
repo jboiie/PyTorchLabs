e7420131e272e4cff5de816dd8b0cfc228c7f665
JAI <jai19kharb@gmail.com>
2025-10-05 00:01:57 +0530

    module 4 done

---
+++ dataset_csv_loader.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+
+class StudentCSVLoader(Dataset):
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
+        self.feature_cols = ["Age", "StudyHours", "Marks"]
+        self.data["Label"] = self.data["Passed"].map({"Yes": 1, "No": 0})
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        features = self.data.loc[idx, self.feature_cols].values.astype('float32')
+        label = self.data.loc[idx, "Label"]
+        return torch.tensor(features), torch.tensor(label)
+
+csv_path = r'C:\Programming\Projects\Learn\pytorch\sample_data.csv'
+dataset = StudentCSVLoader(csv_path)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+for batch_idx, (features, labels) in enumerate(dataloader):
+    print(f"Batch {batch_idx}")
+    print("Features:", features)
+    print("Labels:", labels)
