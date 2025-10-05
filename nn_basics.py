ceb5db655c4e3a18d62026506ec17827e87e393a
JAI <jai19kharb@gmail.com>
2025-10-06 00:14:20 +0530

    nn created

--- nn_basics.py
+++ nn_basics.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-
-#feed forward neural network. with one hidden layer.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+#feed forward neural network. with two hidden layer.
 
 class model(nn.Module):
     #input layer(4features of the flower)
@@ -23,4 +24,51 @@ class model(nn.Module):
 torch.manual_seed(41) #picking a manual seed for randomizaation
 
 mod = model()
-print(model)
+#print(model)
+csv_path = r"C:\Programming\Projects\Learn\pytorch\archive\Iris.csv"
+df = pd.read_csv(csv_path)
+
+df['Species'] = df['Species'].replace('Iris-setosa', 0)
+df['Species'] = df['Species'].replace('Iris-versicolor', 1)
+df['Species'] = df['Species'].replace('Iris-virginica', 2)
+
+#print(df.head)
+
+# train test and split now
+X=df.drop(['Id','Species'], axis=1)
+y=df['Species']
+
+X = X.values
+y = y.values
+
+#print(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=41)
+
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+
+y_train = torch.LongTensor(y_train)#y and x labels to tensors.
+y_test = torch.LongTensor(y_test)
+
+#criteria to measure the error
+criterion = nn.CrossEntropyLoss()
+#adam optimizer
+optimizer = torch.optim.Adam(mod.parameters(), lr=0.01)
+
+#print(mod.parameters)
+
+#train the model.
+epochs=100
+losses = []
+for i in range(epochs):
+    y_pred = mod.forward(X_train) # go forward and predict
+    loss =criterion(y_pred, y_train) #predicted vs y_train
+    losses.append(loss.detach().numpy())#keeping track 
+    if i%10==0:
+        print(f'Epoch:{i} and loss: {loss}') #print every ten epochs
+    # back propogation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
