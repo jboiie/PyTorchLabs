c2b9fa19c4dd29a4e6b799bb646aae9fddcac1f8
JAI <jai19kharb@gmail.com>
2025-10-05 23:42:10 +0530

    module 6 doing

--- nn_basics.py
+++ nn_basics.py
@@ -1,9 +1,26 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
 #feed forward neural network. with one hidden layer.
-class SimpleNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(SimpleNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+
+class model(nn.Module):
+    #input layer(4features of the flower)
+    def __init__(self, in_features=4, h1=8, h2=9, out_features=3):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, h1)
+        self.fc2 = nn.Linear(h1, h2)
+        self.out = nn.Linear(h2, out_features)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))#rectified linear unit
+        x = F.relu(self.fc2(x))   
+        x = self.out(x)
+
+        return x
+    
+torch.manual_seed(41) #picking a manual seed for randomizaation
+
+mod = model()
+print(model)
